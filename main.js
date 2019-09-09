9fa95744cd846ba30942786601919ba4a0364032
hmym <40512627+hmym@users.noreply.github.com>
2019-09-10 00:01:33 +0900

    Revert "boxクラスにfloat-left追加"

--- main.js
+++ main.js
@@ -11,40 +11,33 @@
   const dataBlue = item.querySelectorAll('[data-color="blue"]');
   const dataYellow = item.querySelectorAll('[data-color="yellow"]');
 
-  // boxが表示されていたら消す関数
-  function hide_boxes() {
+  
+  function kesu() {
     for (let i = 0; i < box.length; i++) {
       box[i].style.display = 'none';
     }
   }
-  // boxを表示する関数
-  function display_boxes(data) {
-    for (let i = 0; i < data.length; i++) {
-      data[i].style.display = 'block';
-    }
-  }
   
   for(let i = 0; i < ids.length; i++) {
-    // いずれかのボタンがクリックされたら
     ids[i].addEventListener('click', function() {
-      // 表示されているboxがあれば消す
-      hide_boxes();
-      // クリックされたボタンがredだった場合
+      kesu();
+
       if (i === 0 ) {
-        // データ属性にdata-color="red"がセットされているものを表示する
-        display_boxes(dataRed);
-      // クリックされたボタンがgreenだった場合
-      } else if (i === 1) {
-        // データ属性にdata-color="green"がセットされているものを表示する
-        display_boxes(dataGreen);
-      // クリックされたボタンがblueだった場合
-      } else if (i === 2) {
-        // データ属性にdata-color="blue"がセットされているものを表示する
-        display_boxes(dataBlue);
-      // クリックされたボタンがyellowだった場合
-      } else {
-        // データ属性にdata-color="yellow"がセットされているものを表示する\
-        display_boxes(dataYellow);
+        for (let i = 0; i < dataRed.length; i++) {
+          dataRed[i].style.display = 'block';
+        }
+      }else if(i === 1) {
+        for (let i = 0; i < dataGreen.length; i++) {
+          dataGreen[i].style.display = 'block';
+        }
+      }else if(i === 2) {
+        for (let i = 0; i < dataBlue.length; i++) {
+          dataBlue[i].style.display = 'block';
+        }
+      }else{
+        for (let i=0; i<dataYellow.length; i++) {
+          dataYellow[i].style.display = 'block';
+        }
       }
     });
   }
