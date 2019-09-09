0f1e67f673de03ec49df8d70ca181704279eaab5
hmym <chika.hamayama@gmail.com>
2019-09-09 18:20:53 +0900

    リファクタリング、htmlにデータ属性を追加

--- main.js
+++ main.js
@@ -1,67 +1,40 @@
 'use strict';
 
 {
-//  let id = 'red';
   let ids = document.querySelectorAll('[id]');
-  const typeRed = document.getElementsByClassName('type-red');
-  const typeGreen= document.getElementsByClassName('type-green');
-  const typeBlue= document.getElementsByClassName('type-blue');
-  const typeYellow= document.getElementsByClassName('type-yellow');
+  let box = document.getElementsByClassName('box');
+  const dataRed = document.getElementsByClassName('data-red');
+  const dataGreen= document.getElementsByClassName('data-green');
+  const dataBlue= document.getElementsByClassName('data-blue');
+  const dataYellow= document.getElementsByClassName('data-yellow');
+
+  function kesu() {
+    for (let i=0; i<box.length; i++) {
+      box[i].style.display = 'none';
+    }
+  }
   
   for(let i=0; i<ids.length; i++) {
     ids[i].addEventListener('click', function() {
       if (i === 0 ) {
-        for (let i=0; i<typeRed.length; i++) {
-          typeRed[i].style.display = 'block';
-        }
-        for (let i=0; i<typeGreen.length; i++) {
-          typeGreen[i].style.display = 'none';
-        }
-        for (let i=0; i<typeBlue.length; i++) {
-          typeBlue[i].style.display = 'none';
-        }
-        for (let i=0; i<typeYellow.length; i++) {
-          typeYellow[i].style.display = 'none';
+        kesu();
+        for (let i=0; i<dataRed.length; i++) {
+          dataRed[i].style.display = 'block';
         }
       }else if(i === 1) {
-        for (let i=0; i<typeRed.length; i++) {
-          typeRed[i].style.display = 'none';
+        kesu();
+        for (let i=0; i<dataGreen.length; i++) {
+          dataGreen[i].style.display = 'block';
         }
-        for (let i=0; i<typeGreen.length; i++) {
-          typeGreen[i].style.display = 'block';
-        }
-        for (let i=0; i<typeBlue.length; i++) {
-          typeBlue[i].style.display = 'none';
-        }
-        for (let i=0; i<typeYellow.length; i++) {
-          typeYellow[i].style.display = 'none';
-        }
-
       }else if(i === 2) {
-        for (let i=0; i<typeRed.length; i++) {
-          typeRed[i].style.display = 'none';
-        }
-        for (let i=0; i<typeGreen.length; i++) {
-          typeGreen[i].style.display = 'none';
-        }
-        for (let i=0; i<typeBlue.length; i++) {
-          typeBlue[i].style.display = 'block';
-        }
-        for (let i=0; i<typeYellow.length; i++) {
-          typeYellow[i].style.display = 'none';
+        kesu();
+        for (let i=0; i<dataBlue.length; i++) {
+          dataBlue[i].style.display = 'block';
         }
       }else{
-        for (let i=0; i<typeRed.length; i++) {
-          typeRed[i].style.display = 'none';
-        }
-        for (let i=0; i<typeGreen.length; i++) {
-          typeGreen[i].style.display = 'none';
-        }
-        for (let i=0; i<typeBlue.length; i++) {
-          typeBlue[i].style.display = 'none';
-        }
-        for (let i=0; i<typeYellow.length; i++) {
-          typeYellow[i].style.display = 'block';
+        kesu();
+        for (let i=0; i<dataYellow.length; i++) {
+          dataYellow[i].style.display = 'block';
         }
       }
     });
