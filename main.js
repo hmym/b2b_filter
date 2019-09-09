4e7e0605ce64231ebe0f68fd797bb264e374e6ef
hmym <chika.hamayama@gmail.com>
2019-09-09 20:33:06 +0900

    jsにデータ属性を使用、コメント追記

--- main.js
+++ main.js
@@ -2,37 +2,39 @@
 
 {
   let ids = document.querySelectorAll('[id]');
+  
   let box = document.getElementsByClassName('box');
-  const dataRed = document.getElementsByClassName('data-red');
-  const dataGreen= document.getElementsByClassName('data-green');
-  const dataBlue= document.getElementsByClassName('data-blue');
-  const dataYellow= document.getElementsByClassName('data-yellow');
 
+  const item = document.getElementById('item');
+  const dataRed = item.querySelectorAll('[data-color="red"]');
+  const dataGreen = item.querySelectorAll('[data-color="green"]');
+  const dataBlue = item.querySelectorAll('[data-color="blue"]');
+  const dataYellow = item.querySelectorAll('[data-color="yellow"]');
+
+  
   function kesu() {
-    for (let i=0; i<box.length; i++) {
+    for (let i = 0; i < box.length; i++) {
       box[i].style.display = 'none';
     }
   }
   
-  for(let i=0; i<ids.length; i++) {
+  for(let i = 0; i < ids.length; i++) {
     ids[i].addEventListener('click', function() {
+      kesu();
+
       if (i === 0 ) {
-        kesu();
-        for (let i=0; i<dataRed.length; i++) {
+        for (let i = 0; i < dataRed.length; i++) {
           dataRed[i].style.display = 'block';
         }
       }else if(i === 1) {
-        kesu();
-        for (let i=0; i<dataGreen.length; i++) {
+        for (let i = 0; i < dataGreen.length; i++) {
           dataGreen[i].style.display = 'block';
         }
       }else if(i === 2) {
-        kesu();
-        for (let i=0; i<dataBlue.length; i++) {
+        for (let i = 0; i < dataBlue.length; i++) {
           dataBlue[i].style.display = 'block';
         }
       }else{
-        kesu();
         for (let i=0; i<dataYellow.length; i++) {
           dataYellow[i].style.display = 'block';
         }
